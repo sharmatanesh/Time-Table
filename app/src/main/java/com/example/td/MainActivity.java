252cf8da4a5206344de25317df784024e1542975
sharmatanesh <tanesh9532@gmail.com>
2019-06-23 15:46:04 +0530

    Splash Screen added

--- app/src/main/java/com/example/td/MainActivity.java
+++ app/src/main/java/com/example/td/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.td;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +12,24 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private long backtime;
     private LinearLayout l1;
+    private int splashtime=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,Splash.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashtime);
     }
 
     @Override
     public void onBackPressed() {
         if((backtime+1000)>System.currentTimeMillis()){l1=(LinearLayout)findViewById(R.id.lin2);l1.setVisibility(View.VISIBLE);}
-        else if((backtime+2000)>System.currentTimeMillis()){l1=(LinearLayout)findViewById(R.id.lin2);l1.setVisibility(View.GONE);}
         else if((backtime+3000)>System.currentTimeMillis()){super.onBackPressed(); return;}
         else{Toast t=Toast.makeText(this,"Press again to exit",Toast.LENGTH_SHORT); t.show();}
         backtime=System.currentTimeMillis();
